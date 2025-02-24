b014ad5e2839058b20a41516efc614a8f75fe7e3
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-24 11:00:34 +0200

    Update if12.c

--- code4win/if12.c
+++ code4win/if12.c
@@ -1,1 +1,23 @@
-ECHO is on.
+#include <stdio.h>
+
+int main() {
+    int month;
+    printf("Enter the month number: ");
+    scanf("%d", &month);
+    if (month < 1 || month > 12) {
+       printf("invalid input");
+
+    }
+    else {
+        if (month ==2)
+            printf("month num %d has 29 days", month);
+        else if (month == 4 || month == 6 || month == 9 || month == 11  ) {
+            printf("the month num %d has 30 days", month);
+        }
+        else {
+            printf("the month num %d has 31 days", month);
+        }
+    }
+
+    return 0;
+}
