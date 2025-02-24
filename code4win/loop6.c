aef5414f1a51f567d03e4996fd7cd2094b334468
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-24 13:34:20 +0200

    Update loop6.c

--- code4win/loop6.c
+++ code4win/loop6.c
@@ -1,1 +1,20 @@
-ECHO is on.
+#include <stdio.h>
+
+
+int main() {
+   int  n, i;int sum =0;
+
+    printf("enter the value of n: ");
+    scanf("%d", &n);
+    for ( i =1; i <= n; i++) {
+        sum+=i;
+
+
+
+
+    }
+    printf("%d\n", sum);
+
+
+    return 0;
+}
