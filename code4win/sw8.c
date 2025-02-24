c5c00bb1544d1f44a3c99686f794c971cedb6f04
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-24 13:20:57 +0200

    Update sw8.c

--- code4win/sw8.c
+++ code4win/sw8.c
@@ -1,1 +1,31 @@
-ECHO is on.
+#include <stdio.h>
+
+
+int main() {
+    float a, b;
+    char operation;
+    printf("enter the 2 numbers:\n ");
+    scanf("%f %f", &a, &b);
+    printf("Enter the operation: ");
+    scanf(" %c", &operation);
+    switch (operation) {
+        case '+':
+            printf("%.2f + %.2f = %.2f", a, b, a + b);
+        break;
+        case '-':
+            printf("%.2f - %.2f = %.2f", a, b, a - b);
+        break;
+        case '*':
+            printf("%.2f * %.2f = %.2f", a, b, a * b);
+        break;
+        case '/':
+            printf("%.2f / %.2f = %.2f", a, b, a / b);
+        break;
+
+        default:
+            printf("Invalid operation");
+    }
+
+
+    return 0;
+}
