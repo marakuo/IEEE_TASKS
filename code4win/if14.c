5ef898081d1de25dd1da45e476c2651a7e11b34e
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-24 11:06:52 +0200

    Update if14.c

--- code4win/if14.c
+++ code4win/if14.c
@@ -1,1 +1,21 @@
-ECHO is on.
+#include <stdio.h>
+
+int main() {
+    int ang1, ang2, ang3;
+    printf("Enter the 1st angle:\n");
+    scanf("%d", &ang1);
+    printf("Enter the 2nd angle:\n ");
+    scanf(" %d", &ang2);
+    printf("Enter the 3rd angle:\n");
+    scanf(" %d", &ang3);
+
+    if ( (ang1+ang2+ang3) ==180) {
+       printf("the triangle is valid");
+
+    }
+    else {
+        printf("the triangle is invalid");
+    }
+
+    return 0;
+}
