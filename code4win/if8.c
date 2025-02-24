53d7277669af94cd78b1551085945846611e5b06
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-24 10:50:39 +0200

    Update if8.c

--- code4win/if8.c
+++ code4win/if8.c
@@ -1,1 +1,28 @@
-ECHO is on.
+#include <stdio.h>
+
+int main() {
+    char letter;
+
+
+    printf("Enter a letter: ");
+    scanf("%c", &letter);
+
+    
+
+
+    if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' ||letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U') {
+        printf("%c is a vowel.\n", letter);
+    }
+
+    else if ((letter < 'Z' && letter > 'A') || (letter < 'z' && letter > 'a')) {
+        printf("%c is not a vowel.\n", letter);
+    }
+
+    else {
+
+        printf("%c is an invalid input.\n", letter);
+
+    }
+
+    return 0;
+}
