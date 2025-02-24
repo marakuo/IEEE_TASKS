eef4fe0570b8160dfdba34d117067e34707377aa
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-24 12:37:02 +0200

    Update if20.c

--- code4win/if20.c
+++ code4win/if20.c
@@ -1,1 +1,23 @@
-ECHO is on.
+
+#include <stdio.h>
+
+int main()
+{
+    int sal;
+    printf("Enter the basic salary: ");
+    scanf("%d",&sal);
+    if(sal<= 10000) {
+        printf(" HRA = 20%%, DA = 80%% ");
+    }
+    else if (2000>sal>= 10000) {
+
+        printf(" HRA = 25%%, DA = 90%%");
+
+    }
+    else if (sal >= 20001) {
+
+        printf(" HRA = 30%%, DA = 95%% ");
+
+    }
+    return 0;
+}
