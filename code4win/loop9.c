a331f465acd0f1b1c946cfaa76bb02cd15f20abc
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-24 13:44:15 +0200

    Update loop9.c

--- code4win/loop9.c
+++ code4win/loop9.c
@@ -1,1 +1,19 @@
-ECHO is on.
+#include <stdio.h>
+
+
+int main() {
+   int  n, i;
+
+    printf("enter the value of n: ");
+    scanf("%d", &n);
+    for ( i =1; i <= n; i++) {
+        printf(" %d * %d = %d\n",n ,i,  n*i);
+
+
+
+    }
+
+
+
+    return 0;
+}
