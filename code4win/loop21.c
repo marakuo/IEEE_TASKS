b37282ccc9aacc166f552ad3bd06593de69693d0
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-24 14:21:46 +0200

    Update loop21.c

--- code4win/loop21.c
+++ code4win/loop21.c
@@ -1,1 +1,22 @@
-ECHO is on.
+/**
+ * C program to find power of any number using for loop
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int base , power;
+    long long result=1 ;
+    printf("enter the base: ");
+    scanf("%d", &base);
+    printf("enter the power: ");
+    scanf(" %d", &power);
+    for(int i=1 ; i<=power ;i++){
+        result = result *base;
+        
+    }
+    printf(" %d ^ %d = %lld", base, power, result);
+    
+    return 0;
+}
