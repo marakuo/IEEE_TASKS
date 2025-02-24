bb8d11b8ac3392f4125982a6254a92de138d1bc7
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-24 20:01:49 +0200

    Update loop40.c

--- code4win/loop40.c
+++ code4win/loop40.c
@@ -1,1 +1,24 @@
-ECHO is on.
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    long long binaryNumber;
+    int decimalNumber = 0, i = 0, remainder;
+
+   
+    printf("Enter a binary number: ");
+    scanf("%lld", &binaryNumber);
+
+    
+    while (binaryNumber != 0) {
+        remainder = binaryNumber % 10;  
+        binaryNumber /= 10;            
+        decimalNumber += remainder * pow(2, i);  
+        ++i;
+    }
+
+    
+    printf("Decimal equivalent: %d\n", decimalNumber);
+
+    return 0;
+}
