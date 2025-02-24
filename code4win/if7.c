ae919db95bec20e446a467f04b6a8192aa85602a
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-24 10:27:13 +0200

    Update if7.c

--- code4win/if7.c
+++ code4win/if7.c
@@ -1,1 +1,14 @@
-ECHO is on.
+#include <stdio.h>
+
+int main() {
+    char letter;
+    printf("Enter a letter: ");
+    scanf("%c", &letter);
+    if ((letter >='a' && letter <='z') || (letter >='A' && letter <='Z')) {
+        printf("%c is an alphabet ", letter);
+    }
+    else
+        printf("%c is not an alphabet", letter);
+
+    return 0;
+}
