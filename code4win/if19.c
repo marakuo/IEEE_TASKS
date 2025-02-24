5db7123e1e755b34dfce2539c6442a1c05f554de
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-24 12:14:26 +0200

    Update if19.c

--- code4win/if19.c
+++ code4win/if19.c
@@ -1,1 +1,46 @@
-ECHO is on.
+
+#include <stdio.h>
+
+int main()
+{
+    int ph, chem, bio, math, comp;
+    int percent;
+
+
+    printf("Enter the marks of the 5 subjects: ");
+    scanf("%d%d%d%d%d", &ph, &chem, &bio, &math, &comp);
+
+
+
+    percent = (ph + chem + bio + math + comp) / 5;
+    printf("percentage = %d\n", percent);
+
+
+
+    if(percent >= 90)
+    {
+        printf("Grade A");
+    }
+    else if(percent >= 80)
+    {
+        printf("Grade B");
+    }
+    else if(percent >= 70)
+    {
+        printf("Grade C");
+    }
+    else if(percent >= 60)
+    {
+        printf("Grade D");
+    }
+    else if(percent >= 40)
+    {
+        printf("Grade E");
+    }
+    else
+    {
+        printf("Grade F");
+    }
+
+    return 0;
+}
