af87693c78db5f4ce2922d9cd1883d16ec5a4992
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-24 11:15:59 +0200

    Update if16.c

--- code4win/if16.c
+++ code4win/if16.c
@@ -1,1 +1,29 @@
-ECHO is on.
+#include <stdio.h>
+
+int main() {
+    int s1, s2, s3;
+    printf("Enter the 1st side:\n");
+    scanf("%d", &s1);
+    printf("Enter the 2nd side:\n ");
+    scanf(" %d", &s2);
+    printf("Enter the 3rd side:\n");
+    scanf(" %d", &s3);
+
+
+   if (s1+s2<s3 || s2+s3<s1 || s3+s1<s2) {
+       printf("invalid dimensions"); //check if it's a triangle or not 
+   }
+    else {
+        if (s1 == s2 && s2 == s3) {
+            printf("this is an Equilateral triangle");
+        }
+        else if (s1 == s2 || s2 == s3 || s3==s1) {
+            printf("this is an Isosceles triangle");
+        }
+        else {
+            printf("this is an Escalene triangle");
+        }
+    }
+
+    return 0;
+}
