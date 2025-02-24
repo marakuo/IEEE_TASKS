77eeea6ede85141674a78a2e520a0da9340a4f77
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-24 10:20:32 +0200

    Update if4.c

--- code4win/if4.c
+++ code4win/if4.c
@@ -1,1 +1,17 @@
-ECHO is on.
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    if (num % 5==0 && num % 11==0) {
+        printf("%d is divisible by 11 and 5", num);
+
+    }
+    else {
+        printf(" %d is not divisible by 11 and 5 ", num);
+    }
+
+
+    return 0;
+}
