3f3f4c6d0ca2a3ce6b9c14162669b260fe5fade1
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-24 13:32:06 +0200

    Update loop5.c

--- code4win/loop5.c
+++ code4win/loop5.c
@@ -1,1 +1,17 @@
-ECHO is on.
+#include <stdio.h>
+
+
+int main() {
+   int  n, i;
+    printf("enter the value of n: ");
+    scanf("%d", &n);
+    for ( i =1; i <= n; i+=2) {
+        printf("%d\n", i);
+
+
+    }
+
+
+
+    return 0;
+}
