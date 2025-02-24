5dfdce9f05ed0e045ce52c5a91b5caba4b8042fa
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-24 13:56:49 +0200

    Update loop10.c

--- code4win/loop10.c
+++ code4win/loop10.c
@@ -1,1 +1,15 @@
-ECHO is on.
+#include <stdio.h>
+
+int main() {
+    int num, count=0;
+    printf("enter the num: ");
+    scanf("%d", &num);
+    while (num != 0){
+        num /=10;
+        count++;
+        
+    }
+    printf("%d", count);
+
+    return 0;
+}
