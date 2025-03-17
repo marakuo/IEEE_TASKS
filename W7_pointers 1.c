a3959da39c5b18bf35ab7fed36d49a37346eea96
Malak Abdelhamed <marakuoo82@gmail.com>
2025-03-17 13:18:15 +0200

    Update W7_pointers 1.c

--- W7_pointers 1.c
+++ W7_pointers 1.c
@@ -1,2 +1,32 @@
-/ CODE FOR WIN
-//P1
+/*Hacker Rank*/
+#include <stdio.h>
+
+// function
+void func(int *a, int *b) {
+    int sum = *a + *b;
+    int abs =0;
+    if (*a> *b) {
+        abs = *a - *b ;
+    }
+    else if (*b > *a) {
+        abs = *b - *a;
+    }
+    else
+        abs =0;
+    *a = sum;
+    *b = abs;
+
+}
+
+int main() {
+    int  x, y;
+    printf("Enter x & y :\t");
+    scanf("%d%d", &x, &y);
+    func(&x, &y);
+    printf("x=%d, y=%d\n", x, y);
+
+
+    return 0;
+}
+
+
