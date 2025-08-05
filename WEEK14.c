0bc8fa43dbaa57b3fd457dc70ed769991d807866
Malak Abdelhamed <marakuoo82@gmail.com>
2025-08-05 23:43:33 +0300

    Update WEEK14.c

--- WEEK14.c
+++ WEEK14.c
@@ -198,6 +198,68 @@ int main() {
 
 
 //________________________________________________//
+// INFIX TO PROFIX IMPLEMENTATION 
+ #include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+char stack[20];
+int top =-1;
+void push(char x) {
+    stack[++top] = x;
+
+}
+char pop() {
+    if (top == -1) {
+        return -1;
+    }
+    else
+       return stack[top--];
+}
+
+int priority(char x) {
+    if (x =='(')
+        return 0;
+    else if (x == '+' || x == '-')
+        return 1;
+    else if (x =='*' || x == '/')
+        return 2;
+
+}
+
+int main() {
+    char expression[20];
+    char *ptr, x;
+    printf("Enter expression: ");
+    scanf("%s", expression);
+    ptr=expression;
+    while (*ptr!='\0') {
+        if (isalnum(*ptr)) {
+            printf("%c", *ptr);
+        }else if (*ptr == '(')
+            push(*ptr);
+        else if (*ptr == ')') {
+            while ((x=pop()) !='(')
+                printf("%c", x);
+        }else {
+            while (priority(stack[top]) >= priority(*ptr))
+                printf("%c", pop());
+            push(*ptr);
+        }
+        ptr++;
+    }
+    while (top != -1) {
+        printf("%c", pop());
+    }
+
+
+    return 0;
+}
+
+
+///________________________________///
+// PROFIX TO INFIX IMPLEMENTAION
+
 
 
 
